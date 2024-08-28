71c29aa22deada090529a729b2a91de5119b208e
Francisco <eng.franciscodias@gmail.com>
2024-08-28 16:15:40 -0300

    ghfjfghjh

--- config/database.php
+++ config/database.php
@@ -92,16 +92,13 @@ return [
             'database' => env('DB_DATABASE_UNITRINUS', 'forge'),
             'username' => env('DB_USERNAME_UNITRINUS', 'forge'),
             'password' => env('DB_PASSWORD_UNITRINUS', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+           
+            'charset' => 'utf8',
+            
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
         ],
 
         'pgsql' => [
